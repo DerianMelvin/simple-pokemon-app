47e3fe14ee4c881820af09c796e7f4f2c1b8e0cd
Derian Melvin <derianmelvin88@gmail.com>
2023-09-14 15:37:25 +0700

    feat: add search bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [searchBar, setSearchBar] = useState<string>("");
+
+  const fetchPokemonData = () => {
+    console.log(searchBar);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <main className="w-full p-5 flex flex-col items-center justify-center">
+      <div className="w-full h-48 absolute bg-sky-400 -z-10"></div>
+      <div className="w-full h-36 max-w-7xl flex items-center justify-center bg-green-400">
+        {/* Searchbar */}
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            fetchPokemonData();
+          }}
+          className="w-full flex flex-col items-center justify-center gap-2 sm:flex-row"
+        >
+          <div className="w-1/2 min-w-fit">
+            <label htmlFor="searchbar" className="sr-only">
+              Search Pokemon
+            </label>
+            <input
+              type="text"
+              name="searchbar"
+              placeholder="Search for..."
+              className="w-full px-5 py-2 rounded-full bg-slate-50"
+              onChange={(e) => {
+                setSearchBar(e.target.value);
+              }}
+            />
+          </div>
+          <button
+            type="submit"
+            className="px-4 py-2 rounded-full bg-red-500 text-white"
+          >
+            Search
+          </button>
+        </form>
+
+        {/* Pokemon Sprite */}
+        {/* Pokemon Name */}
+
+        {/* Pokemon description */}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </main>
+  );
 }
 
-export default App
+export default App;
