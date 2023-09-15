33d8c528113f895a71972d0cd64eb1fee41c5bb3
Derian Melvin <derianmelvin88@gmail.com>
2023-09-15 16:08:57 +0700

    feat: update loading & initial page content

--- src/components/PokemonContainerEmpty.tsx
+++ src/components/PokemonContainerEmpty.tsx
@@ -1,6 +1,22 @@
+import PokemonBackground from "./icons/PokemonBackground";
+import PokemonLoading from "./icons/PokemonLoading";
 
 export default function PokemonContainerEmpty() {
   return (
-    <div>Pokemon Container Empty</div>
-  )
+    <div className="w-full flex flex-col items-center justify-center gap-6">
+      <div className="w-full relative flex flex-col items-center justify-center gap-3">
+        <div className="w-1/2 absolute top-0 -z-10 opacity-10 sm:w-fit">
+          <PokemonBackground size={600} />
+        </div>
+      </div>
+
+      <div className="min-w-[200px] min-h-[200px] w-1/4 flex items-center justify-center lg:min-w-[246px] lg:min-h-[246px] xl:min-w-[320px] xl:min-h-[320px]">
+        <PokemonLoading size={60} />
+      </div>
+      <span className="text-2xl lg:text-3xl">No Pokemon in sight</span>
+      <span className="text-lg font-light text-center lg:text-xl">
+        Try searching for your favourite Pokemon (ex. Pikachu)
+      </span>
+    </div>
+  );
 }
--- src/components/PokemonContainerLoading.tsx
+++ src/components/PokemonContainerLoading.tsx
@@ -1,7 +1,19 @@
+import PokemonBackground from "./icons/PokemonBackground";
+import PokemonLoading from "./icons/PokemonLoading";
+
 export default function PokemonContainerLoading() {
   return (
-    <div>
-      <span>Loading...</span>
+    <div className="w-full flex flex-col items-center justify-center gap-6">
+      <div className="w-full relative flex flex-col items-center justify-center gap-3">
+        <div className="w-1/2 absolute top-0 -z-10 opacity-10 sm:w-fit">
+          <PokemonBackground size={600} />
+        </div>
+      </div>
+
+      <div className="min-w-[200px] min-h-[200px] w-1/4 flex items-center justify-center animate-spin lg:min-w-[246px] lg:min-h-[246px] xl:min-w-[320px] xl:min-h-[320px]">
+        <PokemonLoading size={60} />
+      </div>
+      <span className="text-2xl lg:text-4xl">Searching...</span>
     </div>
   );
 }
